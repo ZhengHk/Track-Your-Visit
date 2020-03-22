e5bd2f9927b7cd01bba9b4c7349bc980400b7d41
Liruxo <46451644+Liruxo@users.noreply.github.com>
2020-03-23 00:51:45 +0100

    Firevirus cleanup & comments

--- firevirus/functions/src/index.ts
+++ firevirus/functions/src/index.ts
@@ -2,98 +2,45 @@ import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 admin.initializeApp()
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-// export const testOnChange = functions.firestore.document('realtest/test1').onUpdate(change => {
-
-//     console.info("CHANGE")
-
-//     const after = change.after.data()
-
-//     if (after) {
-//         console.info("Aqr")
-//         const payload = {
-//             notification: {
-//                 title: 'Du hast eine neue Freundschaftsanfrage',
-//                 body: 'Tippe um sie Dir anzeigen zu lassen',
-//             },
-//             data: {
-//                 click_action: 'FLUTTER_NOTIFICATION_CLICK',
-//                 type: 'request',
-//             }
-//         };
-//                 console.info(payload)
-//                 return admin.messaging().sendToTopic("test", payload)
-
-//                 //sendToTopic("notification-update", payload)
-//                 .catch(error => {
-//                     console.error("FCM failed", error)
-//                 })
-//     } else {
-
-//         console.error("You failed")
-//         return null
-//     }
-// })
-
+//listen for newly created infection chains
 export const notificationPush = functions.firestore.document('affected-query/{document}').onCreate(change => {
 
     const after = change.data()
-
+    //check for existence
     if (after) {
-        //console.info("Sending")
-        //console.info(after)
 
-        // const payload = {
-        //     notification: {
-        //         title: 'Du hast ein hohes Ansteckungsrisiko',
-        //         body: 'Aufgrund eines möglichen Kontaktes mit einem Infizierten raten wir dir zuhause zu bleiben',
-        //     },
-        //     data: {
-        //         percentage: "null",
-        //         click_action: 'FLUTTER_NOTIFICATION_CLICK',
-        //     }
-        // };
-        //console.info(payload)
-        console.info(after.constructor.name, "test16")
-
-        //console.info(after.Ahk8tfi3zyeR6ZPuoTNONY4XUSz2)
-
-        //const promises = []
-
+        //iterate over all infected users
         Object.entries(after).forEach(entry => {
+            
+            //userid
             const key = entry[0];
+            
+            //infections percentage
             const value = entry[1];
-            //use key and value here
-            //console.info("ver1", key, value)
 
+            //get fcm-token for userid
             const user = admin.firestore().doc(`fcm-tokens/${key}`).get()
-            //                    promises.push(user)
 
             user.then(token => {
-                //const per = value
                 const data = token.data()
+
+                //check if there is a token entry for this userid
                 if (data) {
-                    console.info(key, "Payload creation", value)
+                    
                     const payload = {
                         notification: {
                             title: 'Du hast ein hohes Ansteckungsrisiko',
-                            body: 'Aufgrund eines möglichen Kontaktes mit einem Infizierten raten wir dir zuhause zu bleiben',
+                            body: 'Aufgrund eines möglichen Kontaktes mit einem Infizierten raten wir dir zuhause zu bleiben.',
                         },
                         data: {
                             percentage: value.toString(),
                             click_action: 'FLUTTER_NOTIFICATION_CLICK'
                         }
                     };
-                    console.info(key, data.token, payload)
+                    
+                    //send message using the token
                     return admin.messaging().sendToDevice(data.token, payload)
-                    //return admin.messaging().sendToTopic("test", payload)
-                    //console.info("Send successfull")
+
                 } else {
                     console.error("Error: Document data for key is empty: ", key)
                     return null
@@ -101,70 +48,14 @@ export const notificationPush = functions.firestore.document('affected-query/{do
             }
             ).catch(error => {
                 console.error(error)
+                return null
             })
-
-
-
+            
         })
-
-        //  return Promise.all(promises)
-        // after.forEach((value: boolean, key: string) => {
-        //     console.log(key, value);
-        // });
-
-        // const promises = []
-        // for (const user in after) {
-        //     const perc = 
-        // }
-
-        //return admin.messaging().sendToDevice(affected-query, payload)
-        //.sendToTopic("test", payload)
-
-        //.catch(error => {
-        //   console.error("FCM failed", error)
-        //})
         return null
+        
     } else {
-
-        console.error("You failed")
+        console.error("Error: Document data for ", change.id, " is emnpty")
         return null
     }
 })
-
-// export const onChange = functions.firestore.document('users/5S70b94wMbgaGI39ZHQaGme58HG3').onUpdate(change => {
-// //    const after = change.after.data()
-//     console.info("CHANGE")
-//     console.error("TEST?")
-
-// // if (after !== null) {
-// //     console.info("Trying send")
-// //     const payload = {
-// //         data : {
-// //             vorname : String(after.vorname),
-// //             clickaction : "FLUTTER_NOTIFICATION_CLICK",
-// //             conditions: after.conditions
-// //         }
-// //     }
-// //     return admin.messaging().sendToTopic("notification-update", payload)
-// //     .catch(error => {
-// //         console.error("FCM failed", error)
-// //     })
-// // }
-//     return null
-// })
-
-//  export const betterWorld = functions.https.onRequest((request, response) => {
-//   admin.firestore().doc('users/5S70b94wMbgaGI39ZHQaGme58HG3').get()
-//   .then(snapshot => {
-//       const data = snapshot.data()
-//       response.send(data)
-//   })
-//   .catch(error => {
-//     //Handle the error
-//     console.log(error)
-//     response.status(500).send(error)
-//   })
-
-
-
-//  })
